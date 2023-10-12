c43ed49b010b4c57aa222480debeb403059d9f03
OlgaCoin <eneyatakara@gmail.com>
2023-10-12 23:51:14 +0300

    header

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,14 @@
 $(document).ready(function () {
 
+	$(window).scroll(function () {
+		var scroll = $(window).scrollTop();
 
+		if (scroll >= 70) {
+			$(".header").addClass("header--fixed");
+		} else {
+			$(".header").removeClass("header--fixed");
+		};
+	});
 
 });
 
