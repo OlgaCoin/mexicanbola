cf1f5b45a9addb8f22968d9edf442bdd737a5efa
OlgaCoin <eneyatakara@gmail.com>
2023-12-03 02:24:31 +0300

    order page

--- src/js/script.js
+++ src/js/script.js
@@ -144,6 +144,45 @@ $(document).ready(function () {
 		]
 	});
 
+	$('.recent__slider').slick({
+		speed: 800,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		// autoplay: true,
+		prevArrow: $('.r-prev'),
+		nextArrow: $('.r-next'),
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 576,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
+	});
+
 	$('.package__slider').slick({
 		speed: 800,
 		slidesToShow: 4,
@@ -222,13 +261,111 @@ $(document).ready(function () {
 		]
 	});
 
-	$(document).ready(function () {
-		$('.filter__form').change(function () {
-			$("#width_tmp_option").html($('.filter__form option:selected').text());
-			$(this).width($("#width_tmp_select").width());
-		});
+	// catalog sorting
+	$('.filter__form').change(function () {
+		$("#width_tmp_option").html($('.filter__form option:selected').text());
+		$(this).width($("#width_tmp_select").width());
 	});
 
+	// product counter
+	function inputNumber(e) {
+
+		var el = e;
+		el.each(function () {
+			el = $(this);
+
+			var min = el.attr('min') || false;
+			var max = el.attr('max') || false;
+
+			var els = {};
+
+			els.dec = el.parents(".count").find(".minus");
+			els.inc = el.parents(".count").find(".plus");
+
+			el.each(function () {
+				init($(this));
+			});
+
+			function init(el) {
+
+				els.dec.on('click', decrement);
+				els.inc.on('click', increment);
+
+				function decrement() {
+					var value = el[0].value;
+					value--;
+					if (!min || value >= min) {
+						el[0].value = value;
+					}
+				}
+
+				function increment() {
+					var value = el[0].value;
+					value++;
+					if (!max || value <= max) {
+						el[0].value = value++;
+					}
+				}
+			}
+		})
+	}
+	inputNumber($('.count__input'));
+
+	// tabs
+	const tabItems = document.querySelectorAll('.tabs__item');
+	const tabContentItems = document.querySelectorAll('.tabs-content__item');
+
+	function selectItem(e) {
+		removeBorder();
+		removeShow();
+		this.classList.add('tab--border');
+		const tabContentItem = document.querySelector(`#${this.id}-content`);
+		tabContentItem.classList.add('show');
+	}
+
+	function removeBorder() {
+		tabItems.forEach(item => {
+			item.classList.remove('tab--border');
+		});
+	}
+
+	function removeShow() {
+		tabContentItems.forEach(item => {
+			item.classList.remove('show');
+		});
+	}
+
+	tabItems.forEach(item => {
+		item.addEventListener('click', selectItem);
+	});
+
+	// Modals
+	if (typeof HystModal !== "undefined") {
+		var xMod = new HystModal({
+			linkAttributeName: 'data-hystmodal',
+			catchFocus: true,
+			backscroll: false,
+			afterClose: function (modal) {
+				// 	If Youtube video inside Modal, close it on modal closing:
+
+				// 	let videoframe = modal.openedWindow.querySelector('iframe');
+				// 	if (videoframe) {
+				// 		videoframe.contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*');
+				// 	}
+			}
+		});
+	}
+
+	function stopVideoHandler(evt) {
+		var link = evt.target;
+		var video = link.parentNode.querySelector("video");
+		video.pause();
+	}
+	function addCloseHandler(closeLink) {
+		closeLink.addEventListener("click", stopVideoHandler);
+	}
+	var closeLinks = document.querySelectorAll(".hystmodal__close, .hystmodal__wrap");
+	closeLinks.forEach(addCloseHandler);
 
 });
 
